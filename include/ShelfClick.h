61adeb45e33c7e4c5cadefb9f439e1b1b7928368
MacGyverr <6500800+MacGyverr@users.noreply.github.com>
2023-01-28 16:15:05 -0800

    Update ShelfClick.h

--- include/ShelfClick.h
+++ include/ShelfClick.h
@@ -14,6 +14,9 @@ struct WeatherAPI {
     char latitude[15];// = 47.698560;
     char longitude[15];// = -122.101970;
     char apikey[50];// = "680e541ac65981bd1835f7728a185c47";
+    char latitude[15];
+    char longitude[15];
+    char apikey[50];
 };
 
 struct Time {
